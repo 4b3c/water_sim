4913f154b66c747e992814a0b9635cf2bca4aa40
Abram <58718191+4b3c@users.noreply.github.com>
2023-10-08 21:21:27 -0700

    made it look like a waving cloth

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import pygame, math, numpy
 
 pygame.init()
 
-window = pygame.display.set_mode((1000, 600))
+window = pygame.display.set_mode((1000, 800))
 running = True
 displacement = 0
 
+def direction_amount(x):
+	if x % 3 == 0:
+		return -1
+	else:
+		return x % 2
+
 def two_dim_graph_wave(position):
 	pixel_array = pygame.PixelArray(window)
-	for x in range(999):
+	for x in range(1000):
 		h = int(math.e**math.sin((x + position) / 100) * 30)
 		h -= int(math.e**math.sin((x - (position * 2 / 7)) / 30) * 10)
 		pixel_array[x][400 - h] = (0, 0, 0)
@@ -16,39 +22,39 @@ def two_dim_graph_wave(position):
 
 def two_dim_flat_graph_wave(position):
 	pixel_array = pygame.PixelArray(window)
-	for x in range(999):
+	for x in range(1000):
 		h = int(math.e**math.sin((x + position) / 100) * 30)
 		h -= int(math.e**math.sin((x - (position * 2 / 7)) / 30) * 10)
-		pixel_array[x][0:600] = (h + 100, h + 100, h + 100)
+		pixel_array[x][0:800] = (h + 100, h + 100, h + 100)
 	pixel_array.close()
 
 def two_dim_graph_wave_more_waves(position):
 	pixel_array = pygame.PixelArray(window)
-	for x in range(999):
+	for x in range(1000):
 		h = 0
 		for y in range(1, 12):
-			h -= math.e**math.sin(((x + (position * -(y % 2) * (30/(y+1)))) * y) / 300) * 31 / (y + 0.1)
+			h -= math.e**math.sin(((x + (position * direction_amount(y) * (30/(y+1)))) * y) / 300) * 31 / (y + 0.1)
 
 		clamp = min(max(-h, 0), 255)
 		clamp2 = 255 - clamp
 
 		pixel_array[x][0:400 + int(h)] = (clamp2/3, clamp2/1.9, clamp2/1.4)
-		pixel_array[x][400 + int(h):600] = (clamp/3, clamp/1.9, clamp/1.4)
+		pixel_array[x][400 + int(h):800] = (clamp/3, clamp/1.9, clamp/1.4)
 
 	pixel_array.close()
 
 def more_waves_with_derivative(position):
 	pixel_array = pygame.PixelArray(window)
-	for x in range(999):
+	for x in range(1000):
 		h = 0
 		dh = 0
 		for y in range(1, 8):
-			c = math.e**math.sin(((x + (position * -(y % 2) * (30/(y+1)))) * y) / 300) * 31 / (y + 0.1)
+			c = math.e**math.sin(((1.2 * x + (position * direction_amount(y) * (30/(y+1))) ) * y) / 300) * 33 / (y + 0.1)
 			h -= c
-			dh -= c * y * y * math.cos(((x + (position * -(y % 2) * (30/(y+1)))) * y) / 300) * 31 / (y + 0.1)
+			dh -= c * y * y * math.cos(((1.2 * x + (position * direction_amount(y) * (30/(y+1)))) * y) / 300) * 33 / (y + 0.1)
 
 		derivative_clamp = min(max(dh / 50, -125), 100) + 155
-		pixel_array[x][200 + int(h):600] = (derivative_clamp*(35/255), derivative_clamp*(137/255), derivative_clamp*(218/255))
+		pixel_array[x][250 + int(h):650 + int(h)] = (derivative_clamp*(35/255), derivative_clamp*(137/255), derivative_clamp*(218/255))
 
 
 
