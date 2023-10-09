2d4b238817965bdf064ba3252f727b8b2f75559b
Abram <58718191+4b3c@users.noreply.github.com>
2023-10-08 21:47:39 -0700

    made a vertical version before i do full 2d

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def two_dim_graph_wave(position):
 	for x in range(1000):
 		h = int(math.e**math.sin((x + position) / 100) * 30)
 		h -= int(math.e**math.sin((x - (position * 2 / 7)) / 30) * 10)
-		pixel_array[x][400 - h] = (0, 0, 0)
+		pixel_array[x, 400 - h] = (0, 0, 0)
 	pixel_array.close()
 
 def two_dim_flat_graph_wave(position):
@@ -25,7 +25,7 @@ def two_dim_flat_graph_wave(position):
 	for x in range(1000):
 		h = int(math.e**math.sin((x + position) / 100) * 30)
 		h -= int(math.e**math.sin((x - (position * 2 / 7)) / 30) * 10)
-		pixel_array[x][0:800] = (h + 100, h + 100, h + 100)
+		pixel_array[x, 0:800] = (h + 100, h + 100, h + 100)
 	pixel_array.close()
 
 def two_dim_graph_wave_more_waves(position):
@@ -38,8 +38,8 @@ def two_dim_graph_wave_more_waves(position):
 		clamp = min(max(-h, 0), 255)
 		clamp2 = 255 - clamp
 
-		pixel_array[x][0:400 + int(h)] = (clamp2/3, clamp2/1.9, clamp2/1.4)
-		pixel_array[x][400 + int(h):800] = (clamp/3, clamp/1.9, clamp/1.4)
+		pixel_array[x, 0:400 + int(h)] = (clamp2/3, clamp2/1.9, clamp2/1.4)
+		pixel_array[x, 400 + int(h):800] = (clamp/3, clamp/1.9, clamp/1.4)
 
 	pixel_array.close()
 
@@ -53,12 +53,13 @@ def more_waves_with_derivative(position):
 			position_mod = position * direction_amount(y) * (30 / (y + 1))
 			x_mod = x * 1.2
 			y_mod = 33 / (y + 0.1)
+
 			c = math.e**math.sin((x_mod + position_mod) * (y / 300)) * y_mod
 			h -= c
 			dh -= c * y * y * math.cos((x_mod + position_mod) * (y / 300)) * y_mod
 
 		derivative_clamp = min(max(dh / 60, -125), 100) + 155
-		pixel_array[x][250 + int(h):650 + int(h)] = (derivative_clamp*(35/255), derivative_clamp*(137/255), derivative_clamp*(218/255))
+		pixel_array[x, 250 + int(h):650 + int(h)] = (derivative_clamp*(35/255), derivative_clamp*(137/255), derivative_clamp*(218/255))
 
 
 		#failed to make it look like it had a fixed length
@@ -66,14 +67,29 @@ def more_waves_with_derivative(position):
 		# if total_dh >= 70:
 		# 	break
 
+def vertical_banner(position):
+	pixel_array = pygame.PixelArray(window)
+	for x in range(800):
+		h = 0
+		dh = 0
+		for y in range(1, 8):
+			position_mod = position * direction_amount(y) * (30 / (y + 1))
+			x_mod = x * 1.2
+			y_mod = 33 / (y + 0.1)
+	
+			c = math.e**math.sin((x_mod + position_mod) * (y / 300)) * y_mod
+			h -= c
+			dh -= c * y * y * math.cos((x_mod + position_mod) * (y / 300)) * y_mod
 
+		derivative_clamp = min(max(dh / 60, -125), 100) + 155
+		pixel_array[250 + int(h):850 + int(h), x] = (derivative_clamp*(35/255), derivative_clamp*(137/255), derivative_clamp*(218/255))
 
 
 while running:
 	window.fill((15, 45, 95));
 
 	displacement += 0.3
-	more_waves_with_derivative(displacement)
+	vertical_banner(displacement)
 
 	pygame.display.update()
 	for event in pygame.event.get():
