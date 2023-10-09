76b5fe2df3112fa2e5d3a17ef4a808e59946375e
Abram <58718191+4b3c@users.noreply.github.com>
2023-10-08 20:50:43 -0700

    added shadow using the derivative

--- main.py
+++ main.py
@@ -26,17 +26,29 @@ def two_dim_graph_wave_more_waves(position):
 	pixel_array = pygame.PixelArray(window)
 	for x in range(999):
 		h = 0
-		for y in range(12):
+		for y in range(1, 12):
 			h -= math.e**math.sin(((x + (position * -(y % 2) * (30/(y+1)))) * y) / 300) * 31 / (y + 0.1)
 
-		# pixel_array[x][800 + int(h)] = (0, 0, 0)
-		clamp = min(max(-h-300, 0), 255)
+		clamp = min(max(-h, 0), 255)
 		clamp2 = 255 - clamp
 
-		pixel_array[x][0:700 + int(h)] = (clamp2/3, clamp2/1.9, clamp2/1.4)
-		pixel_array[x][700 + int(h):600] = (clamp/3, clamp/1.9, clamp/1.4)
+		pixel_array[x][0:400 + int(h)] = (clamp2/3, clamp2/1.9, clamp2/1.4)
+		pixel_array[x][400 + int(h):600] = (clamp/3, clamp/1.9, clamp/1.4)
 
 	pixel_array.close()
+
+def more_waves_with_derivative(position):
+	pixel_array = pygame.PixelArray(window)
+	for x in range(999):
+		h = 0
+		dh = 0
+		for y in range(1, 8):
+			c = math.e**math.sin(((x + (position * -(y % 2) * (30/(y+1)))) * y) / 300) * 31 / (y + 0.1)
+			h -= c
+			dh -= c * y * y * math.cos(((x + (position * -(y % 2) * (30/(y+1)))) * y) / 300) * 31 / (y + 0.1)
+
+		derivative_clamp = min(max(dh / 50, -125), 100) + 155
+		pixel_array[x][200 + int(h):600] = (derivative_clamp*(35/255), derivative_clamp*(137/255), derivative_clamp*(218/255))
 
 
 
@@ -45,7 +57,7 @@ while running:
 	window.fill((15, 45, 95));
 
 	displacement += 0.3
-	two_dim_graph_wave_more_waves(displacement)
+	more_waves_with_derivative(displacement)
 
 	pygame.display.update()
 	for event in pygame.event.get():
